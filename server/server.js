78207f3875a07c4dece2d1b4833db87190a603cc
shahin <142988554+shahinahaammed@users.noreply.github.com>
2024-03-24 04:16:21 +0530

    server login route

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+app.use(express.json());
+
+const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'https://localhost:3000',
+    clientId: 'b8a8d6f9fe554b2186fce973d53eac25',
+    clientSecret: 'e8a86331dc5a44829c8830e1c8fd3e18'
+});
+
+app.post('/login', (req, res) => {
+    const code = req.body.code;
+    
+    spotifyApi.authorizationCodeGrant(code)
+        .then(data => {
+            res.json({
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                expiresIn: data.body.expires_in
+            });
+        })
+        .catch(err => {
+            console.error('Error getting tokens:', err);
+            res.sendStatus(400);
+        });
+});
