8f1b54c916afa3eee825ac2e9dd530fb8248bc6c
shahin <142988554+shahinahaammed@users.noreply.github.com>
2024-03-25 10:19:13 +0530

    search

--- server/server.js
+++ server/server.js
@@ -9,6 +9,28 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(express.json());
 
+
+app.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'https://localhost:3000',
+        clientId: 'b8a8d6f9fe554b2186fce973d53eac25',
+        clientSecret: 'e8a86331dc5a44829c8830e1c8fd3e18',
+        refreshToken
+    });
+
+    spotifyApi.refreshAccessToken()
+        .then(data => {
+            res.json({
+                accessToken: data.body.access_token,
+                expiresIn: data.body.expiresIn
+            })
+        })
+        .catch(() => {
+            res.sendStatus(400);
+        });
+});
+
 const spotifyApi = new SpotifyWebApi({
     redirectUri: 'https://localhost:3000',
     clientId: 'b8a8d6f9fe554b2186fce973d53eac25',
